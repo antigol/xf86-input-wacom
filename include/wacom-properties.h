c80bee29fbdd9dac0ec1b5d451d44bcf33a1fdac
Mario Geiger <geiger.mario@gmail.com>
2015-08-26 20:44:17 +0200

    ...

--- include/wacom-properties.h
+++ include/wacom-properties.h
@@ -27,11 +27,11 @@
 /* 32 bit, 4 values, top x, top y, bottom x, bottom y */
 #define WACOM_PROP_TABLET_AREA "Wacom Tablet Area"
 
-/* 32 bit, 4 values, border width, border offset, point physical, point logical */
-#define WACOM_PROP_TABLET_DISTORTION_TOP_X "Wacom Tablet Border Distortion Top X"
-#define WACOM_PROP_TABLET_DISTORTION_TOP_Y "Wacom Tablet Border Distortion Top Y"
-#define WACOM_PROP_TABLET_DISTORTION_BOTTOM_X "Wacom Tablet Border Distortion Bottom X"
-#define WACOM_PROP_TABLET_DISTORTION_BOTTOM_Y "Wacom Tablet Border Distortion Bottom Y"
+/* 32 bit, 5 values, border width, polynomial coefficient x^3, x^2, x, 1 */
+#define WACOM_PROP_TABLET_DISTORTION_TOP_X "Wacom Border Distortion Top X"
+#define WACOM_PROP_TABLET_DISTORTION_TOP_Y "Wacom Border Distortion Top Y"
+#define WACOM_PROP_TABLET_DISTORTION_BOTTOM_X "Wacom Border Distortion Bottom X"
+#define WACOM_PROP_TABLET_DISTORTION_BOTTOM_Y "Wacom Border Distortion Bottom Y"
 
 /* 8 bit, 1 value, [0 - 3] (NONE, CW, CCW, HALF) */
 #define WACOM_PROP_ROTATION "Wacom Rotation"
